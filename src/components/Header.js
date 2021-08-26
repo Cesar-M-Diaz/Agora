18f43f50f428899092ca7bee89c755a2a4bd21bf
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-08-26 18:42:32 -0500

    Complete Header without links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Logo from '../assets/images/Logo.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import '../assets/styles/components/Header.css';
 
 class Header extends React.Component{
     render(){
         return(
-            <header>Header</header>
+            <header className="header">
+                <a href="/">
+                    <img className="header__logo" src={Logo} alt="Logo" />
+                </a>
+                <div className="header__search-container">
+                    <input className="search-container__input" type="text" placeholder="Search" />
+                    <div className="search-container__icon-container">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </div>
+                </div>
+                <div className="header__buttons-container">
+                    <button className="button-container__signin-button" type="button">Sign in</button>
+                    <button className="button-container__register-button" type="button">Register</button>
+                </div>
+            </header>
         )
     }
 }
