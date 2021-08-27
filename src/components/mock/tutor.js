1df5d5e550902ea6d274ccb5eb920a091e2b1c6f
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-08-27 17:49:56 -0500

    create tutor mock data

---
+++ src/components/mock/tutor.js
@@ -0,0 +1,8 @@
+import {v4 as uuidv4} from 'uuid' 
+
+export const mockTeachers=[
+    {_id:uuidv4(), name:"felipe", email:"pipe@outlook.es", professiopn:"chemical engineer",schedule:"",profilePhoto:"",fucos:"math",rating:"5",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non."},
+    {_id:uuidv4(), name:"mari", email:"mari@outlook.es",professiopn:"civil engineer",schedule:"",profilePhoto:"",fucos:"chemistry",rating:"4",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
+    {_id:uuidv4(), name:"cesar", email:"cesar@outlook.es",professiopn:"electrical engineer",schedule:"",profilePhoto:"",fucos:"biology",rating:"5",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
+    {_id:uuidv4(), name:"luis", email:"luis@outlook.es",professiopn:"mechanical engineer",schedule:"",profilePhoto:"",fucos:"physics",rating:"4",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",}
+]
