25261e4e2ac3b36e283779883ab7227497f94cf6
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-08-27 17:43:42 -0500

    create categories mock data

---
+++ src/components/mock/categories.js
@@ -0,0 +1,8 @@
+import {v4 as uuidv4} from 'uuid' 
+
+export const mockTeachers=[
+    {_id:uuidv4(), subject:"math",focus:"math",profession:"chemical engineer",tutorId:""},
+    {_id:uuidv4(), subject:"chemistry",focus:"chemistry",profession:"civil engineer",tutorId:""},
+    {_id:uuidv4(), subject:"biology",focus:"biology",profession:"electrical engineer",tutorId:""},
+    {_id:uuidv4(), subject:"physics",focus:"physics",profession:"mechanical engineer",tutorId:""},
+]
