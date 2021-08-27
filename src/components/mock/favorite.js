0f6edb75b4762d61375f1726381c67cd3faa430e
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-08-27 17:46:12 -0500

    create favorite mock data

---
+++ src/components/mock/favorite.js
@@ -0,0 +1,8 @@
+import {v4 as uuidv4} from 'uuid' 
+
+export const mockTeachers=[
+    {_id:uuidv4(), studentId:"ljbl",tutorId:"ruihtb"},
+    {_id:uuidv4(), studentId:"t5b54",tutorId:"gtb"},
+    {_id:uuidv4(), studentId:"54tb",tutorId:"teetg"},
+    {_id:uuidv4(), studentId:"5t4br5",tutorId:"wegt"},
+]
