de489afd10338f8d0c73bcf02223266e9fbebb55
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-08-27 17:49:08 -0500

    create student mock data

---
+++ src/components/mock/student.js
@@ -0,0 +1,8 @@
+import {v4 as uuidv4} from 'uuid' 
+
+export const mockTeachers=[
+    {_id:uuidv4(), name:"ana", email:"aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non."},
+    {_id:uuidv4(), name:"juan", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
+    {_id:uuidv4(), name:"sofia", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
+    {_id:uuidv4(), name:"carlos", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",}
+]
