774fd7e5d974e07085a118888473f81473f0ab60
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-08-27 19:25:34 -0500

    Finish responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../assets/images/Logo.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 import '../assets/styles/components/Header.css';
 
@@ -12,14 +12,27 @@ const currentStudent = mockStudents[0];
 
 class Header extends React.Component{
     state = {
-        isAuth: false,
-        searchInput: ''
+        isAuth: true,
+        searchInput: '',
+        isSearchCollapsed: true,
+        isMenuCollapsed: true
     }
 
     render(){
         const handleChange = e => {
             this.setState({searchInput: e.target.value})
         }
+
+        const toggleSearchCollapse = () => {
+            if(!this.state.isMenuCollapsed) this.setState({isMenuCollapsed: !this.state.isMenuCollapsed});
+            this.setState({isSearchCollapsed: !this.state.isSearchCollapsed});
+        }
+
+        const toggleMenuCollapse = () => {
+            if(!this.state.isSearchCollapsed) this.setState({isSearchCollapsed: !this.state.isSearchCollapsed});
+            this.setState({isMenuCollapsed: !this.state.isMenuCollapsed});
+        }
+
         return(
             <header className="header">
                 <a href="/">
@@ -31,12 +44,41 @@ class Header extends React.Component{
                         <FontAwesomeIcon icon={faSearch} />
                     </div>
                 </div>
+
+                <div className="mobile-nav-buttons-container">
+                        <FontAwesomeIcon onClick={toggleSearchCollapse} icon={faSearch} />
+                        <FontAwesomeIcon onClick={toggleMenuCollapse} icon={faBars} />
+                </div>
+
+                <div className={`mobile-search-input ${!this.state.isSearchCollapsed && 'active'}`}>
+                    <input onChange={handleChange} className="search-container__input" type="text" placeholder="Search" />
+                    <div className="search-container__icon-container">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </div>   
+                </div>
+
+                <div className={`mobile-menu ${!this.state.isMenuCollapsed && 'active'}`}>
+                        {this.state.isAuth ? 
+                            <div className="mobile-menu__profile-photo-container">
+                                <a className="header__profile-photo-link" href="/">
+                                    <img className="header__profile-photo" src={currentStudent.profile_photo} alt="Profile" />
+                                </a>
+                                <span>{currentStudent.name}</span>
+                            </div>
+                            :
+                            <div className="mobile__buttons-container">
+                                <button className="button-container__signin-button" type="button">Sign in</button>
+                                <button className="button-container__register-button" type="button">Register</button>
+                            </div>
+                        
+                        }
+                        {/* SUBJECTS COMPONENT WOULD GO HERE */}
+                </div>
                 
                 {this.state.isAuth ? 
-                    <a href="/">
+                    <a className="header__profile-photo-link" href="/">
                         <img className="header__profile-photo" src={currentStudent.profile_photo} alt="Profile" />
                     </a>
-
                     :
                     <div className="header__buttons-container">
                         <button className="button-container__signin-button" type="button">Sign in</button>
