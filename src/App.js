6958a85a762100bd8c85163b7c04aa09df9c942a
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-09-01 20:34:20 -0500

    Add register route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+import Register from './pages/Register'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </Router>
   );
