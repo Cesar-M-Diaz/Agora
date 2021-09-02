0e1d9f1c2ceeb8e62112db87351307824398698a
Mariana Usuga Montoya <marianausugamontoya12344@gmail.com>
2021-09-01 19:25:55 -0500

    Solve PR

--- src/components/mock/student.js
+++ src/components/mock/student.js
@@ -1,8 +1,8 @@
-import {v4 as uuidv4} from 'uuid' 
+import { v4 as uuidv4 } from 'uuid' 
 
-export const mockTeachers=[
-    {_id:uuidv4(), name:"ana", email:"aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non."},
-    {_id:uuidv4(), name:"juan", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
-    {_id:uuidv4(), name:"sofia", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",},
-    {_id:uuidv4(), name:"carlos", email:"aknifa",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio, voluptatem ea minus illo sequi reiciendis! Ad temporibus quos non.",}
+export const student=[
+    { _id:uuidv4(), name: "ana", email: "aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio" },
+    { _id:uuidv4(), name: "juan", email: "aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio" },
+    { _id:uuidv4(), name: "sofia", email: "aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio" },
+    { _id:uuidv4(), name: "carlos", email: "aknifa", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo reprehenderit fugiat aut quis, voluptate sit laborum quia voluptas minima distinctio" },
 ]
