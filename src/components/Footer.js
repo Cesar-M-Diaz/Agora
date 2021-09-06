2d20764310dd353254c64c4172f6a684515b8e8b
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-09-06 18:08:40 -0500

    solve identation problems

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faFacebookSquare, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebookSquare,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
-import '../assets/styles/components/Footer.css';
+import "../assets/styles/components/Footer.css";
 
-function Footer(){
-    return(
-        <footer className="footer">
-            <div className="footer__copy">
-                <span>© 2021 Agora</span>
-            </div>
-            <div className="footer__message">
-                <span>Made with love in Make it Real</span>
-            </div>
-            <div className="footer__social">
-                <a href="/">
-                    <FontAwesomeIcon icon={faInstagram} />
-                </a>
-                <a href="/">
-                    <FontAwesomeIcon icon={faFacebookSquare} />
-                </a>
-                <a href="/">
-                    <FontAwesomeIcon icon={faWhatsapp} />
-                </a>                    
-            </div>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__copy">
+        <span>© 2021 Agora</span>
+      </div>
+      <div className="footer__message">
+        <span>Made with love in Make it Real</span>
+      </div>
+      <div className="footer__social">
+        <a href="/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="/">
+          <FontAwesomeIcon icon={faFacebookSquare} />
+        </a>
+        <a href="/">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
