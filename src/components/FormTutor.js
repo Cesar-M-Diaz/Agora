481619c3320053faf0f7d524a25eda4285171384
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2021-09-09 19:10:06 -0500

    Refactor register

--- src/components/FormTutor.js
+++ src/components/FormTutor.js
@@ -1,14 +1,38 @@
-const FormTutor = ({ addData }) => {
+const FormTutor = ({ handleChange, validateInputs, errors }) => {
   return (
     <>
-      <div className="form__inputs">
-        <input type="text" placeholder="Profession" name="Profession" onChange={addData} />
+      <div className="register-form__inputs">
+        <input
+          onBlur={validateInputs}
+          type="text"
+          placeholder="Profession"
+          name="profession"
+          onChange={handleChange}
+        />
       </div>
-      <div className="form__inputs">
-        <input type="text" placeholder="Focus" name="Focus" onChange={addData} />
+      <span>{errors.profession}</span>
+      <div className="register-form__inputs">
+        <select
+          onBlur={validateInputs}
+          type="text"
+          placeholder="Focus"
+          name="focus"
+          onChange={handleChange}
+          defaultValue={0}
+        >
+          <option value={0} hidden disabled>
+            Choose a focus
+          </option>
+          <option value={1}>Test option 1</option>
+          <option value={2}>Test option 2</option>
+          <option value={3}>Test option 3</option>
+          <option value={4}>Test option 4</option>
+          <option value={5}>Test option 5</option>
+        </select>
       </div>
+      <span>{errors.focus}</span>
     </>
-  )
-}
+  );
+};
 
 export default FormTutor;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,69 +1,260 @@
-import { render } from '@testing-library/react';
-import React, { Fragment } from 'react'
-import register from '../assets/styles/pages/register.css'
-import { FaUserAlt, FaEnvelope, FaKey } from "react-icons/fa";
-import { useState } from 'react'
+import { useState } from 'react';
+import { FaUserAlt, FaEnvelope, FaKey } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import FormTutor from '../components/FormTutor';
-import { Link, link } from 'react-router-dom';
-import { appendErrors, useForm } from 'react-hook-form'
+
+import '../assets/styles/pages/register.scss';
 
 function Register() {
-  const [form, setForm] = useState({ role: 'student' })
+  const [state, setState] = useState({
+    inputs: {
+      type: 'student',
+      name: '',
+      email: '',
+      password: '',
+      profession: '',
+      focus: '',
+    },
+    errors: {
+      name: true,
+      email: true,
+      password: true,
+      profession: true,
+      focus: true,
+    },
+    isValid: false,
+  });
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  function validateInputs(e) {
+    const inputName = e.target.name;
+    if (inputName === 'name') {
+      if (e.target.value.length < 4) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            name: 'Name is too short',
+          },
+          isValid: false,
+        }));
+        return;
+      }
+      const re = /^[a-zA-Z\s]*$/;
+      if (re.test(String(e.target.value).toLowerCase())) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: { ...prevState.errors, name: '' },
+          isValid:
+            prevState.inputs.type === 'student'
+              ? !(state.errors.email || state.errors.password)
+              : !(
+                  state.errors.email ||
+                  state.errors.password ||
+                  state.errors.profession ||
+                  state.errors.focus
+                ),
+        }));
+      } else {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            name: 'Your name must only contain letters',
+          },
+        }));
+      }
+    }
+    if (inputName === 'email') {
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (re.test(String(e.target.value).toLowerCase())) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: { ...prevState.errors, email: '' },
+          isValid:
+            prevState.inputs.type === 'student'
+              ? !(state.errors.password || state.errors.name)
+              : !(
+                  state.errors.password ||
+                  state.errors.name ||
+                  state.errors.profession ||
+                  state.errors.focus
+                ),
+        }));
+      } else {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            email: 'Invalid email, please enter a valid email',
+          },
+          isValid: false,
+        }));
+        return;
+      }
+    }
+    if (inputName === 'password') {
+      if (e.target.value.length < 4) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            password: 'Invalid password, the password is too short',
+          },
+          isValid: false,
+        }));
+        return;
+      } else {
+        setState((prevState) => ({
+          ...prevState,
+          errors: { ...prevState.errors, password: '' },
+          isValid:
+            prevState.inputs.type === 'student'
+              ? !(state.errors.email || state.errors.name)
+              : !(
+                  state.errors.email ||
+                  state.errors.name ||
+                  state.errors.profession ||
+                  state.errors.focus
+                ),
+        }));
+      }
+    }
+    if (inputName === 'profession') {
+      if (e.target.value.length < 4) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            profession: 'Profession name is too short',
+          },
+          isValid: false,
+        }));
+        return;
+      }
+      const re = /^[a-zA-Z\s]*$/;
+      if (re.test(String(e.target.value).toLowerCase())) {
+        setState((prevState) => ({
+          ...prevState,
+          errors: { ...prevState.errors, profession: '' },
+          isValid: !(
+            state.errors.email ||
+            state.errors.password ||
+            state.errors.name ||
+            state.errors.focus
+          ),
+        }));
+      } else {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            profession: 'Your profession name must only contain letters',
+          },
+        }));
+      }
+    }
+    if (inputName === 'focus') {
+      if (e.target.value !== '0') {
+        setState((prevState) => ({
+          ...prevState,
+          errors: {
+            ...prevState.errors,
+            focus: '',
+          },
+          isValid: !(
+            state.errors.email ||
+            state.errors.password ||
+            state.errors.name ||
+            state.errors.profession
+          ),
+        }));
+      }
+    }
+  }
 
-  function onSubmit(){
-    console.log(form)
-  }
-  const addData = (e) => {
-    setForm((form) => ({...form, [e.target.name]: e.target.value }))
-  }
+  function handleSubmit() {}
+  const handleChange = (e) => {
+    setState((state) => ({
+      ...state,
+      inputs: { ...state.inputs, [e.target.name]: e.target.value },
+    }));
+  };
+
   return (
-    <Fragment>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <h2>Register</h2>
-        <div className="form__inputs">
-          <FaUserAlt className="form__icon" />
-          <input type="text" placeholder="Name" name="name"
-            {...register('name', {
-              required: 'This is required',
-              pattern: { value: /\s*([A-Za-z]+)+\s*/, message: "Your name must have only letters" },
-            })}
-            onChange={addData} />
-        </div>
-        {errors.name && <p className="form__v">{errors.name.message}</p>}
-        <div className="form__inputs">
-          <FaEnvelope className="form__icon" />
-          <input type="text" placeholder="Email" name="email"
-            {...register('email', {
-              required: 'This is required',
-              pattern: { value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/, message: " Your email is incorrect" },
-            })}
-            onChange={addData} />
-        </div>
-        {errors.email && <p className="form__v">{errors.email.message}</p>}
-        <div className="form__inputs">
-          <FaKey className="form__icon" />
-          <input type="password" placeholder="Password" name="password"
-            {...register('password', {
-              required: 'This is required',
-              minLength: { value: 8, message: 'minimum password of 8 characters' }
-            })}
-            onChange={addData} />
-        </div>
-        {errors.password && <p className="form__v">{errors.password.message}</p>}
-        <div className="form__choose-role">
-          <h5 className="form__t-s">Do you want being student or tutor?</h5>
-          <select name='role' onChange={addData}>
+    <>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <h2 className="register-form__title">Register</h2>
+
+        <div className="register-form__choose-role">
+          <h5 className="register-form__t-s">Are you a student or a tutor?</h5>
+          <select
+            name="type"
+            onChange={handleChange}
+            className="register-form__dropdown"
+          >
             <option>student</option>
             <option>tutor</option>
           </select>
         </div>
-        <p className="form__tutor">{form.role === 'tutor' ? <FormTutor addData={addData} /> : ""}</p>
-        <button type="submit" className="form__button">Register</button>
-        <p className="form__account">Do you already have an account? <Link to="/sign in">Sign in</Link></p>
+
+        <div className="register-form__inputs">
+          <FaUserAlt className="register-form__icon" />
+          <input
+            onBlur={validateInputs}
+            onChange={handleChange}
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+          />
+        </div>
+        <span>{state.errors.name}</span>
+
+        <div className="register-form__inputs">
+          <FaEnvelope className="register-form__icon" />
+          <input
+            onBlur={validateInputs}
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <span>{state.errors.email}</span>
+        <div className="register-form__inputs">
+          <FaKey className="register-form__icon" />
+          <input
+            onBlur={validateInputs}
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <span>{state.errors.password}</span>
+        {state.inputs.type === 'tutor' && (
+          <FormTutor
+            handleChange={handleChange}
+            validateInputs={validateInputs}
+            errors={{
+              profession: state.errors.profession,
+              focus: state.errors.focus,
+            }}
+          />
+        )}
+        <button
+          type="submit"
+          className={`register-form__button ${!state.isValid && 'disabled'}`}
+          disabled={!state.isValid}
+        >
+          Register
+        </button>
+        <p className="register-form__account">
+          Do you already have an account? <Link to="/sign in">Sign in</Link>
+        </p>
       </form>
-    </Fragment>
-  )
+    </>
+  );
 }
 export default Register;
