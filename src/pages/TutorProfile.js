8bb6d3b123de855f7ad3f692e125fa8c4e90bbf3
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2021-09-20 16:34:26 -0500

    add useEffect to Tutor Profile

--- src/pages/TutorProfile.js
+++ src/pages/TutorProfile.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TutorPageHead } from '../components/TutorPageHead';
 import { TutorDescription } from '../components/TutorDescription';
 import { tutor as tutors } from '../components/mock/tutor';
 import { ReviewsContainer } from '../components/ReviewsContainer';
 import { reviews } from '../components/mock/reviews';
 import '../assets/styles/pages/TutorProfile.css';
+import axios from 'axios';
 
-function TutorProfile() {
-  const tutor = tutors[0];
+function TutorProfile(props) {
+  const [tutor, setTutor] = useState({});
+
+  useEffect(() => {
+    axios.get('/tutor/:id', { params: props.id }).then((response) => {
+      const data = response.data.tutorData;
+      setTutor(data);
+    });
+  }, [tutor]);
 
   return (
     <div className="tutor-profile__body">
