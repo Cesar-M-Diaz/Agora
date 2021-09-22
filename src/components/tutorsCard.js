63f1a7b646ac48b256e0123dbad052312af61d03
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-09-22 16:39:53 -0500

    get information from backend to TutorsContainer component

--- src/components/tutorsCard.js
+++ src/components/tutorsCard.js
@@ -2,25 +2,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import '../assets/styles/components/tutorCards.scss';
 
-function TutorsCard(props) {
-  const tutors = props;
+function TutorsCard(tutor) {
   const starNodes = [];
 
-  for (let i = 1; i <= tutors.rating; i++) {
+  for (let i = 1; i <= tutor.props.rating; i++) {
     starNodes.push(<FontAwesomeIcon icon={faStar} />);
   }
+
 
   return (
     <div className="card">
       <div className="card-visual-info">
-        <img src={tutors.profPic} alt="profilepicture"></img>
-        <h1>{tutors.name}</h1>
+        <img src={tutor.props.profile_photo} alt="profilepicture"></img>
+        <h1>{tutor.props.name}</h1>
         <div className="card-stars">{starNodes}</div>
       </div>
       <div className="card-tutor-info">
-        <h2>{tutors.profession}</h2>
-        <h3>{tutors.focus}</h3>
-        <p>{tutors.description}</p>
+        <h2>{tutor.props.profession}</h2>
+        <h3>{tutor.props.focus}</h3>
+        <p>{tutor.props.description}</p>
       </div>
     </div>
   );
