c077f8c590e8c1bec99b320de782847da13add4c
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-09-24 14:36:23 -0500

    destructuring props

--- src/components/tutorsCard.js
+++ src/components/tutorsCard.js
@@ -3,24 +3,24 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import '../assets/styles/components/tutorCards.scss';
 
 function TutorsCard(tutor) {
+  const {rating, profile_photo, name, profession, focus, description, _id} = tutor.props
   const starNodes = [];
 
-  for (let i = 1; i <= tutor.props.rating; i++) {
+  for (let i = 1; i <= rating; i++) {
     starNodes.push(<FontAwesomeIcon icon={faStar} />);
   }
 
-
   return (
-    <div className="card">
+    <div className="card" key={_id}>
       <div className="card-visual-info">
-        <img src={tutor.props.profile_photo} alt="profilepicture"></img>
-        <h1>{tutor.props.name}</h1>
+        <img src={profile_photo} alt="profilepicture"></img>
+        <h1>{name}</h1>
         <div className="card-stars">{starNodes}</div>
       </div>
       <div className="card-tutor-info">
-        <h2>{tutor.props.profession}</h2>
-        <h3>{tutor.props.focus}</h3>
-        <p>{tutor.props.description}</p>
+        <h2>{profession}</h2>
+        <h3>{focus}</h3>
+        <p>{description}</p>
       </div>
     </div>
   );
