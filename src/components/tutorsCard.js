bc83bbad01700a5e6007ba05da8f4eef8a08b31f
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2021-09-25 17:39:25 -0500

    Prepare front end to vercel deployment

--- src/components/tutorsCard.js
+++ src/components/tutorsCard.js
@@ -1,17 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import '../assets/styles/components/tutorCards.scss';
+import history from '../utils/history';
 
 function TutorsCard(tutor) {
-  const {rating, profile_photo, name, profession, focus, description, _id} = tutor.props
+  const { rating, profile_photo, name, profession, focus, description, _id } =
+    tutor.props;
   const starNodes = [];
 
   for (let i = 1; i <= rating; i++) {
     starNodes.push(<FontAwesomeIcon icon={faStar} />);
   }
 
+  function handleClick() {
+    history.push({
+      pathname: '/tutor',
+      state: _id,
+    });
+  }
+
   return (
-    <div className="card" key={_id}>
+    <div className="card" key={_id} onClick={handleClick}>
       <div className="card-visual-info">
         <img src={profile_photo} alt="profilepicture"></img>
         <h1>{name}</h1>
