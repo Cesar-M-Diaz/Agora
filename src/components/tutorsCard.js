e6f73dd1ee73500578168e1466f459e382f41f18
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-09-30 18:56:24 -0500

    add tutorscontainer test an solve keys problem in its components

--- src/components/tutorsCard.js
+++ src/components/tutorsCard.js
@@ -9,7 +9,7 @@ function TutorsCard(tutor) {
   const starNodes = [];
 
   for (let i = 1; i <= rating; i++) {
-    starNodes.push(<FontAwesomeIcon icon={faStar} />);
+    starNodes.push(<FontAwesomeIcon key={i} icon={faStar} />);
   }
 
   function handleClick() {
@@ -20,14 +20,14 @@ function TutorsCard(tutor) {
   }
 
   return (
-    <div className="card" key={_id} onClick={handleClick}>
+    <div key={_id} className="card"  onClick={handleClick} >
       <div className="card-visual-info">
         <img src={profile_photo} alt="profilepicture"></img>
-        <h1>{name}</h1>
-        <div className="card-stars">{starNodes}</div>
+        <h1 key={name}>{name}</h1>
+        <div className="card-stars" >{starNodes}</div>
       </div>
       <div className="card-tutor-info">
-        <h2>{profession}</h2>
+        <h2 key = {profession}>{profession}</h2>
         <h3>{focus}</h3>
         <p>{description}</p>
       </div>
