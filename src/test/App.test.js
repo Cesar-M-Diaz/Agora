6529640dce31b0b754cc5b3aafcacf00e0fa0001
cesar diaz <arq.cmdiaz1@gmail.com>
2021-10-02 16:37:43 -0500

    Fix login test and app test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,46 +4,64 @@ import history from '../utils/history';
 import createStore from '../store';
 import App from '../App';
 import { MemoryRouter } from 'react-router-dom';
+import axios from '../utils/axios';
 
 jest.mock('../utils/axios.js');
 
 let store;
 beforeEach(() => {
+  window.scrollTo = jest.fn();
   localStorage.clear();
   store = createStore();
 });
 
-// technical debt
-// test('renders register component', () => {
-//   history.push('/register');
-//   render(
-//     <Provider store={store}>
-//       <MemoryRouter>
-//         <App />
-//       </MemoryRouter>
-//     </Provider>,
-//   );
-//   expect(screen.getByTestId('register-title')).toBeInTheDocument();
-// });
+describe('General Navigation Test', () => {
+  test('renders register component', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: {
+        categories: [
+          { _id: 1, subject: 'Math' },
+          { _id: 2, subject: 'Chemistry' },
+          { _id: 3, subject: 'Biology' },
+          { _id: 4, subject: 'Physics' },
+          { _id: 5, subject: 'Development' },
+          { _id: 6, subject: 'Psychology' },
+        ],
+      },
+    });
 
-test('renders login component', () => {
-  history.push('/login');
-  render(
-    <Provider store={store}>
-      <MemoryRouter>
+    history.push('/register');
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() => screen.getByText('Are you a student or a tutor?'));
+  });
+
+  test('renders login component', () => {
+    history.push('/login');
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(screen.getByTestId('login-title')).toBeInTheDocument();
+  });
+
+  test('redirects to login if not authenticated', () => {
+    history.push('/home');
+    render(
+      <Provider store={store}>
         <App />
-      </MemoryRouter>
-    </Provider>,
-  );
-  expect(screen.getByTestId('login-title')).toBeInTheDocument();
-});
-
-test('redirects to login if not authenticated', () => {
-  history.push('/home');
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
-  expect(screen.getByTestId('login-title')).toBeInTheDocument();
+      </Provider>,
+    );
+    expect(screen.getByTestId('login-title')).toBeInTheDocument();
+  });
 });
