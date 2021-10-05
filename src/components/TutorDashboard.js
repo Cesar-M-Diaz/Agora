e2022bc6be0ae94dee82f99b98fb04ec039754c6
cesar diaz <arq.cmdiaz1@gmail.com>
2021-10-05 15:58:03 -0500

    Modify tutor dashboard with links

--- src/components/TutorDashboard.js
+++ src/components/TutorDashboard.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import '../assets/styles/components/TutorDashboard.scss';
+import history from '../utils/history';
 
 function TutorDashboard() {
+  function handleClick(e) {
+    e.preventDefault();
+    history.push(`/${e.target.name}`);
+  }
+  function handleChange(e) {
+    history.push(`/${e.target.value}`);
+  }
+
   return (
     <main className="tutor-dashboard__body">
-      <select name="page selector" className="tutor-dashboard__selector">
-        <option className="tutor-dashboard__selector-element">Profile</option>
-        <option className="tutor-dashboard__selector-element">
+      <select name="page selector" className="tutor-dashboard__selector" onChange={handleChange}>
+        <option className="tutor-dashboard__selector-element" value="profile">
+          Profile
+        </option>
+        <option className="tutor-dashboard__selector-element" value="tutorship">
           Tutorships
         </option>
-        <option className="tutor-dashboard__selector-element">
+        <option className="tutor-dashboard__selector-element" value="tutorship history">
           Tutorships History
         </option>
       </select>
-      <button className="tutor-dashboard__button">Profile</button>
-      <button className="tutor-dashboard__button">Tutorships</button>
-      <button className="tutor-dashboard__button">Tutorships History</button>
+      <button className="tutor-dashboard__button" onClick={handleClick} name="profile">
+        Profile
+      </button>
+      <button className="tutor-dashboard__button" onClick={handleClick} name="tutorship">
+        Tutorships
+      </button>
+      <button className="tutor-dashboard__button" onClick={handleClick} name="tutorship history">
+        Tutorships History
+      </button>
     </main>
   );
 }
