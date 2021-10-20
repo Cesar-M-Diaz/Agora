cdd3eee45355ef2cd9dda65a2f30702dad44d04a
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-10-20 13:45:04 -0500

    Fix Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
+import '../assets/styles/components/Layout.scss';
+
 class Layout extends React.Component {
   render() {
     return (
       <>
         <Header />
-        {this.props.children}
+        <div className="LayoutContainer">
+          {this.props.children}
+        </div>
         <Footer />
       </>
     );
