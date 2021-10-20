0584f43b1fc5a1acf45aafaf4a1572955251f559
cesar diaz <arq.cmdiaz1@gmail.com>
2021-10-20 17:01:29 -0500

    Fix tutor profile routes

--- src/App.js
+++ src/App.js
@@ -17,8 +17,6 @@ import ProfileRouteComponent from './utils/ProfileRouteComponent';
 import { SearchPage } from './pages/searchPage';
 import ScrollToTop from './utils/ScrollToTop';
 import TutorsSchedule from './pages/TutorsSchedule';
-import TutorshipPage from './pages/TutorshipPage';
-import TutorshipHistoy from './pages/TutorshipHistory';
 import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
@@ -45,11 +43,8 @@ function App() {
           <Route exact path="/search" component={SearchPage} />
           <Route exact path="/error" component={errorPage} />
           <PrivateRoute exact path="/profile/:section" component={ProfileRouteComponent} />
-          <PrivateRoute exact path="/profile" component={ProfileRouteComponent} />
           <Route exact path="/tutor/:id" component={TutorDetailsPage} />
           <PrivateRoute exact path="/tutor/:id/schedule" component={TutorsSchedule} />
-          <PrivateRoute exact path="/create-tutorship" component={TutorshipPage} />
-          <PrivateRoute exact path="/tutorship-history" component={TutorshipHistoy} />
           <PrivateRoute exact path="/checkout/:id" component={CheckoutPage} />
           <Route path="*" component={errorPage} />
         </Switch>
