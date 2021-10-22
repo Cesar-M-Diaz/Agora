e5a0d176f954a4c53dd52a9dff516b638941c2af
cesar diaz <arq.cmdiaz1@gmail.com>
2021-10-22 14:10:20 -0500

    Implement add card functionality

--- src/components/PaymentMethods.js
+++ src/components/PaymentMethods.js
@@ -5,13 +5,22 @@ import Loader from '../components/Loader';
 import CreditCard from '../components/CreditCard';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import history from '../utils/history';
 import '../assets/styles/pages/checkout.scss';
 
 function PaymentMethods() {
-  const MySwal = withReactContent(Swal);
   const user_id = useSelector((state) => state.currentUser._id);
+  const user_email = useSelector((state) => state.currentUser.email);
+  const user_name = useSelector((state) => state.currentUser.name);
+  const firstName = user_name.split(' ')[0];
+  const lastName = user_name.split(' ')[1];
+  const MySwal = withReactContent(Swal);
+  const [paymentInfo, setPaymentInfo] = useState({
+    doc_type: '',
+    doc_number: '',
+  });
   const [cardName, setCardName] = useState({
-    card_name: '',
+    card_name: user_name,
   });
   const [hidden, setHidden] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
@@ -26,9 +35,27 @@ function PaymentMethods() {
     'card[cvc]': '',
   });
   const [customerInfo, setCustomerInfo] = useState({
-    name: '',
-    last_name: '',
-    email: '',
+    name: firstName,
+    last_name: lastName,
+    email: user_email,
+  });
+  const [errors, setErrors] = useState({
+    doc_type: '',
+    doc_number: '',
+    'card[number]': '',
+    'card[exp_year]': '',
+    'card[exp_month]': '',
+    'card[cvc]': '',
+    card_name: '',
+  });
+  const [isValid, setIsValid] = useState({
+    doc_type: false,
+    doc_number: false,
+    'card[number]': false,
+    'card[exp_year]': false,
+    'card[exp_month]': false,
+    'card[cvc]': false,
+    card_name: true,
   });
 
   useEffect(() => {
@@ -46,6 +73,10 @@ function PaymentMethods() {
     });
   }, [epayco_customer_id, user_id]);
 
+  function selectCard(card) {
+    console.log(card.token);
+  }
+
   function cardInfoChange(e) {
     setCardInfo((state) => ({
       ...state,
@@ -60,9 +91,79 @@ function PaymentMethods() {
     }));
   }
 
+  function paymentInfoChange(e) {
+    setPaymentInfo((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function validateinputs(e) {
+    const input = e.target.name;
+    const value = e.target.value;
+    const textRegex = /^[a-zA-Z\s]*$/;
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (
+      !value &&
+      (input === 'card[exp_month]' ||
+        input === 'card[exp_year]' ||
+        input === 'card[cvc]' ||
+        input === 'doc_type' ||
+        input === 'doc_number')
+    ) {
+      setErrors((state) => ({
+        ...state,
+        [input]: 'this fields are mandatory, please fill each one of them',
+      }));
+      setIsValid((state) => ({
+        ...state,
+        [input]: false,
+      }));
+    } else if (!value) {
+      setErrors((state) => ({
+        ...state,
+        [input]: 'this field is mandatory',
+      }));
+      setIsValid((state) => ({
+        ...state,
+        [input]: false,
+      }));
+    } else if (input === 'card_name' && !textRegex.test(String(e.target.value).toLowerCase())) {
+      setErrors((state) => ({
+        ...state,
+        [input]: 'field must only contain letters',
+      }));
+      setIsValid((state) => ({
+        ...state,
+        [input]: false,
+      }));
+    } else if (input === 'email' && !emailRegex.test(String(value).toLowerCase())) {
+      setErrors((state) => ({
+        ...state,
+        [input]: 'please enter a valid email',
+      }));
+      setIsValid((state) => ({
+        ...state,
+        [input]: false,
+      }));
+    } else {
+      setErrors((state) => ({
+        ...state,
+        [input]: '',
+      }));
+      setIsValid((state) => ({
+        ...state,
+        [input]: true,
+      }));
+    }
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
     setIsLoading(true);
+    setHidden(true);
     try {
       if (epayco_customer_id) {
         await axios.post('/create-card', {
@@ -71,27 +172,27 @@ function PaymentMethods() {
         });
       } else {
         await axios.post('/create-user', {
+          user_id,
           customerInfo,
           cardInfo,
         });
       }
-      await axios.get(`/get-customer?id=${user_id}`).then((result) => {
-        const customer = result.data.customer.data;
-        setEpayco_customer_id(customer.id_customer);
-        setExistingCards((prevState) => ({
-          ...prevState,
-          cards: customer.cards,
-        }));
-        setIsLoading(false);
-      });
       MySwal.fire({
         icon: 'success',
         title: <p className="swal__tittle">Card Created</p>,
         confirmButtonColor: '#0de26f',
+      }).then(() => {
+        history.go(0);
       });
     } catch (err) {
-      console.log(err);
       setIsLoading(false);
+      const errorMessage = err.response.data;
+      MySwal.fire({
+        icon: 'error',
+        title: <p className="swal__tittle">Oops... Please try again</p>,
+        text: errorMessage,
+        confirmButtonColor: '#ce4c4c',
+      });
     }
   }
 
@@ -104,6 +205,15 @@ function PaymentMethods() {
       'card[cvc]': '',
     });
     setCardName({
+      card_name: '',
+    });
+    setErrors({
+      doc_type: '',
+      doc_number: '',
+      'card[number]': '',
+      'card[exp_year]': '',
+      'card[exp_month]': '',
+      'card[cvc]': '',
       card_name: '',
     });
   }
@@ -122,8 +232,11 @@ function PaymentMethods() {
       ) : (
         <div>
           <h2 className="payment__credit-card-title">My Credit Cards</h2>
+          <button onClick={() => setHidden(false)} hidden={!hidden}>
+            add card
+          </button>
           {existingCards.cards.map((card) => {
-            return <CreditCard card={card} key={card.token} />;
+            return <CreditCard card={card} key={card.token} selectCard={selectCard} />;
           })}
         </div>
       )}
@@ -132,24 +245,52 @@ function PaymentMethods() {
         <form action="" className="payment__form" onSubmit={handleSubmit}>
           <div className="payment__form-slot">
             <label>name on card</label>
-            <input type="text" name="card_name" value={cardName.card_name} onChange={cardNameChange} />
-            <span className="payment__errors"></span>
+            <input
+              type="text"
+              name="card_name"
+              value={cardName.card_name}
+              onChange={cardNameChange}
+              onBlur={validateinputs}
+            />
+            <span className="payment__errors">{errors.card_name}</span>
           </div>
           <div className="payment__form-slot">
             <label>card number</label>
-            <input type="number" name="card[number]" value={cardInfo['card[number]']} onChange={cardInfoChange} />
-            <span className="payment__errors"></span>
+            <input
+              type="number"
+              name="card[number]"
+              value={cardInfo['card[number]']}
+              onChange={cardInfoChange}
+              onBlur={validateinputs}
+            />
+            <span className="payment__errors">{errors['card[number]']}</span>
           </div>
           <div className="payment__card-form">
             <div className="payment__card-form-slot">
               <label>exp date</label>
-              <input
-                type="number"
+              <select
                 name="card[exp_month]"
-                placeholder="month"
-                value={cardInfo['card[exp_month]']}
+                id="month"
                 onChange={cardInfoChange}
-              />
+                onBlur={validateinputs}
+                value={cardInfo['card[exp_month]']}
+              >
+                <option value={0} hidden>
+                  month
+                </option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+                <option value={8}>8</option>
+                <option value={9}>9</option>
+                <option value={10}>10</option>
+                <option value={11}>11</option>
+                <option value={12}>12</option>
+              </select>
             </div>
             <div className="payment__card-form-slot">
               <label className="payment__hidden">.</label>
@@ -157,17 +298,69 @@ function PaymentMethods() {
                 type="number"
                 name="card[exp_year]"
                 placeholder="year"
-                value={cardInfo['card[exp_year]']}
                 onChange={cardInfoChange}
+                onBlur={validateinputs}
               />
             </div>
             <div className="payment__cvc-form-slot">
               <label>cvc</label>
-              <input type="number" name="card[cvc]" value={cardInfo['card[cvc]']} onChange={cardInfoChange} />
+              <input
+                type="number"
+                name="card[cvc]"
+                value={cardInfo['card[cvc]']}
+                onChange={cardInfoChange}
+                onBlur={validateinputs}
+              />
             </div>
           </div>
-          <span className="payment__errors"></span>
-          <button>save card</button>
+          <span className="payment__errors">
+            {errors['card[cvc]'] || errors['card[exp_year]'] || errors['card[exp_month]']}
+          </span>
+          <div className="payment__card-form">
+            <div className="payment__id-type-form-slot">
+              <label>id type</label>
+              <select
+                name="doc_type"
+                id="doc_type"
+                onChange={paymentInfoChange}
+                value={paymentInfo.doc_type}
+                onBlur={validateinputs}
+              >
+                <option value={0} hidden>
+                  please select
+                </option>
+                <option value="cc">CC</option>
+                <option value="nit">NIT</option>
+              </select>
+            </div>
+            <div className="payment__id-num-form-slot">
+              <label>id number</label>
+              <input
+                name="doc_number"
+                type="number"
+                value={paymentInfo.doc_number}
+                onChange={paymentInfoChange}
+                onBlur={validateinputs}
+              />
+            </div>
+          </div>
+          <span className="payment__errors">{errors.doc_type || errors.doc_number}</span>
+          <button
+            className="payment__pay-button"
+            disabled={
+              !(
+                isValid.doc_type &&
+                isValid.doc_number &&
+                isValid['card[number]'] &&
+                isValid['card[exp_year]'] &&
+                isValid['card[exp_month]'] &&
+                isValid['card[cvc]'] &&
+                isValid.card_name
+              )
+            }
+          >
+            add card
+          </button>
           <button onClick={handleCancel}>cancel</button>
         </form>
       )}
